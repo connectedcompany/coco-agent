fde16915b3ecc3c7ecb145b0f7d612bae2b9c8a5
Uros Rapajic <uros.rapajic@connectedcompany.io>
2021-05-20 16:39:31 +0100

    Updated gitpython and srsly

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 
 config = {
-    "version": "0.1.3",
+    "version": "0.1.4",
     "name": "coco-agent",
     "description": "coco-agent",
     "author": "CoCo",
-    #'url': 'unused',
+    "url": "https://github.com/connectedcompany/coco-agent",
     #'download_url': 'Where to download it.',
     #'author_email': 'My email.',
     "install_requires": [
         "click==7.1.2",
-        "gitpython==3.1.11",
+        "gitpython==3.1.17",
         "google-cloud-storage==1.33.0",
         "pybase62==0.4.3",
-        "srsly==2.3.2",
+        "srsly==2.4.1",
     ],
     "python_requires": ">=3.6",
     "packages": find_packages(),
