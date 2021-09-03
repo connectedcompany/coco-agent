bf4942b8ef5822161fe2e420fa9a36a1ecccd760
Uros Rapajic <uros.rapajic@connectedcompany.io>
2021-09-03 23:37:52 +0100

    update deps

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import find_packages, setup
 
 config = {
-    "version": "0.1.4",
+    "version": "0.1.5",
     "name": "coco-agent",
     "description": "coco-agent",
-    "author": "CoCo",
+    "author": "connectedcompany",
     "url": "https://github.com/connectedcompany/coco-agent",
     #'download_url': 'Where to download it.',
     #'author_email': 'My email.',
     "install_requires": [
         "click==7.1.2",
         "gitpython==3.1.17",
-        "google-cloud-storage==1.33.0",
+        "google-cloud-logging==2.6.0",
+        "google-cloud-storage==1.42.0",
         "pybase62==0.4.3",
         "srsly==2.4.1",
-        "srsly>=1.0",
+        "urllib3>=1.26.6",
     ],
     "python_requires": ">=3.6",
     "packages": find_packages(),
