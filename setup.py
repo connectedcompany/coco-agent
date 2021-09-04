fe51c55d788ccd8818b7eff422c1b757f2a24cfa
Uros Rapajic <uros.rapajic@connectedcompany.io>
2021-09-04 19:12:51 +0000

    Version 0.2.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 config = {
-    "version": "0.2.5",
+    "version": "0.2.6",
     "name": "coco-agent",
     "description": "coco-agent",
     "author": "connectedcompany.io",
